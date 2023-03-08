e8c930d021dac5e6d626de7a4ca442d58186dcd0
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-03-08 13:10:17 -0300

    avances varios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Context } from "./context/Context";
+import Home from "./views/Home";
 
 function App() {
+  fetch("https://pokeapi.co/api/v2/pokemon/?offset=150").then((res)=>res.json().then((res)=>console.log(res)))
   return (
-    <div>
-
-    </div>
+    <Context>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+      </BrowserRouter>
+    </Context>
   );
 }
 
 export default App;
+
+// https://tenor.com/es/search/pikachu-stickers
---
+++ src/context/Context.jsx
@@ -0,0 +1,80 @@
+import { createContext } from 'react';
+
+export const UseContext = createContext();
+
+export const Context = ({ children }) => {
+
+    const colorsTypes=[
+        {
+            name:"electric",
+            color:"#F7C545"
+        },{
+            name:"normal",
+            color:"#A8A090"
+        },{
+            name:"fighting",
+            color:"#A05038"
+        },{
+            name:"flying",
+            color:"#98A8F0"
+        },{
+            name:"poison",
+            color:"#B058A0"
+        },{
+            name:"ground",
+            color:"#E9D6A4"
+        },{
+            name:"rock",
+            color:"	#B8A058"
+        },{
+            name:"bug",
+            color:"#A8B820"
+        },{
+            name:"ghost",
+            color:"#6060B0"
+        },{
+            name:"fire",
+            color:"#F05030"
+        },{
+            name:"water",
+            color:"#3899F8"
+        },{
+            name:"grass",
+            color:"#78C850"
+        },{
+            name:"electric",
+            color:"#F8D030"
+        },{
+            name:"psychic",
+            color:"	#F870A0"
+        },{
+            name:"ice",
+            color:"#58C8E0"
+        },{
+            name:"dragon",
+            color:"#7860E0"
+        },{
+            name:"steel",
+            color:"#A8A8C0"
+        },{
+            name:"dark",
+            color:"#7A5848"
+        },{
+            name:"unknow",
+            color:"grey"
+        },{
+            name:"shadow",
+            color:"#6060B0"
+        },{
+            name:"fairy",
+            color:"#E79FE7"
+        }
+
+    ]
+    
+    return (
+        <UseContext.Provider value={{ colorsTypes }}>
+            {children}
+        </UseContext.Provider>
+    );
+};
