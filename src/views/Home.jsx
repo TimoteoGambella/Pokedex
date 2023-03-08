80cd9c7a31bc58028683a03842e8784e7b28bfeb
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-03-08 20:04:36 -0300

    banner advance

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,9 @@
+import banner from "../assets/banner.svg"
+
 export default function Home(){
     return(
         <div className="home-container">
-            
+            <img src={banner} alt="BANNER" />
         </div>
     )
 }
