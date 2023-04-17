b5bd426ef010f00bd0062b06e8a702f4efcf6733
TimoteoGambella <timi.gambella@hotmail.com>
2023-04-17 09:15:15 -0300

    navegadores

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
 import logo from "../assets/logo.svg"
+import { useNavigate } from "react-router-dom";
 
 export default function NavBar(){
     const [menuBar,setMenuBar]=useState(true)
     const [openMenu,setOpenMenu]=useState(false)
 
+    const navigate=useNavigate()
+
+    const navegador=(param)=>{
+        setOpenMenu(false)
+        navigate(`/${param}`)
+    }
+
     return(
         <div className="navbar-container">
-            <img src={logo} alt="LOGO" />
+            <img onClick={()=>navegador("")} src={logo} alt="LOGO" />
 
             <button className={`hamburger hamburger--stack ${openMenu&&"active"}`} type="button"
                     onClick={()=>{
@@ -33,9 +41,9 @@ export default function NavBar(){
                     </div>
                 </button>
                 <div className={`menu ${openMenu?"open":"close"}`}>
-                    <img src={logo} alt="LOGO" />
+                    <img onClick={()=>navegador("")} src={logo} alt="LOGO" />
                     <div>
-                        <p>Pokedex</p>
+                        <p onClick={()=>navegador("pokedex")}>Pokedex</p>
                         <p>Legendarios</p>
                         <p>Sobre mí</p>
                     </div>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,10 @@
 import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import {  UseApiContext } from "../context/ApiContext";
 import banner from "../assets/banner.svg"
 
 export default function Home(){
+    const navigate=useNavigate()
     const {generation}=useParams()
     const { colorsTypes, apiPoke, allPokes } = useContext(UseApiContext)
 
@@ -26,7 +27,7 @@ export default function Home(){
             <img src={banner} alt="BANNER" />
             <h2><span>Find</span> all your favorite <span>Pokemon</span></h2>
             <h3>You can know the type of Pokemon, its strengths, disadvantages and abilities</h3>
-            <p>See Pokemons</p>
+            <p onClick={()=>navigate("/pokedex")}>See Pokemons</p>
         </div>
     )
 }
