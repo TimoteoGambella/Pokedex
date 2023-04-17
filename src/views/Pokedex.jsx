29ec2782129a9ccde67bfb7870ffa8204905aced
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-04-17 11:00:36 -0300

    poke lupa

--- src/views/Pokedex.jsx
+++ src/views/Pokedex.jsx
@@ -1,27 +1,58 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import {  UseApiContext } from "../context/ApiContext";
-import banner from "../assets/banner.svg"
 import SearchIcon from '@mui/icons-material/Search';
 import { SvgIcon } from "@mui/material";
 import FilterListIcon from '@mui/icons-material/FilterList';
 
-export default function Pokedex(){
+export default function Pokedex({openMenu}){
+    const [openFilters,setOpenFilters]=useState(false)
+
+    const [lupa,setLupa]=useState(false)
+
+    useEffect(() => {
+        if(openMenu){
+            setLupa(true)
+        }else{
+            if(lupa){
+                setTimeout(() => {
+                    setLupa(false)
+                }, 1000);
+            }
+        }
+    }, [openMenu,lupa])
 
     return(
-        <>
             <div className="pokedex-container">
                 <p className="banner">800 <span>Pokemons</span> for you to choose your favorite</p>
                 <div className="buscador">
                     <input type="text" placeholder="Encuentra tu pokemon..."/>
-                    <SvgIcon component={SearchIcon} className="buscador-logo"/>
+                    <SvgIcon component={SearchIcon} className="buscador-logo" sx={{zIndex:lupa?-1:0}}/>
                 </div>
                 <div className="filtros">
-                    <p>Filtros</p>
-                    <SvgIcon component={FilterListIcon} className="filtros-logo"/>
+                    <p onClick={()=>setOpenFilters(!openFilters)}>Filtros</p>
+                    <SvgIcon component={FilterListIcon} className="filtros-logo" onClick={()=>setOpenFilters(!openFilters)}/>
                 </div>
+
+
+                <div className={`filtros-menu ${openFilters?"open":"close"}`}>
+                    <button className={`hamburger hamburger--stack ${openFilters&&"active"}`} type="button"
+                        onClick={()=>{
+                            window.scrollTo({
+                                top: 0,
+                                behavior: "auto",
+                            });
+                            setOpenFilters(!openFilters)
+                        }}
+                    >
+                        <div className="inner">
+                            <span className={`bar ${!lupa&&"bar2"}`}></span>
+                            <span className={`bar ${!lupa&&"bar2"} ${!lupa&&"bar2-background"}`}></span>
+                            <span className={`bar ${!lupa&&"bar2"}`}></span>
+                        </div>
+                    </button>
+                </div>
+                <div className={`fondoAlt ${openFilters?"open":"close"}`} onClick={()=>setOpenFilters(!openFilters)}></div>
             </div>
-            <div className={`filtros-drop`}></div>
-        </>
     )
 }
