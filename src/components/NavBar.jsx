5f927409bba73eb1ba4dab633c49a77934940502
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-04-17 11:00:44 -0300

    navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,9 +2,8 @@ import { useState } from "react";
 import logo from "../assets/logo.svg"
 import { useNavigate } from "react-router-dom";
 
-export default function NavBar(){
+export default function NavBar({openMenu,setOpenMenu}){
     const [menuBar,setMenuBar]=useState(true)
-    const [openMenu,setOpenMenu]=useState(false)
 
     const navigate=useNavigate()
 
@@ -15,40 +14,32 @@ export default function NavBar(){
 
     return(
         <div className="navbar-container">
-            <img onClick={()=>navegador("")} src={logo} alt="LOGO" />
+            <img onClick={()=>navegador("")} src={logo} alt="LOGO"/>
 
             <button className={`hamburger hamburger--stack ${openMenu&&"active"}`} type="button"
-                    onClick={()=>{
-                        window.scrollTo({
-                            top: 0,
-                            behavior: "auto",
-                        });
-                        setOpenMenu(!openMenu)
-                    }}
-                >
-                    <div className="inner"
-                        onClick={()=>{
-                            if(window.location.pathname==="/Contact" && menuBar){
-                                setMenuBar(false)
-                            }else if(window.location.pathname==="/Contact" && !menuBar){
-                                setMenuBar(true)
-                            }
-                        }}
-                    >
-                        <span className={`bar ${menuBar&&"bar2"}`}></span>
-                        <span className={`bar ${menuBar&&"bar2"} ${menuBar&&"bar2-background"}`}></span>
-                        <span className={`bar ${menuBar&&"bar2"}`}></span>
-                    </div>
-                </button>
-                <div className={`menu ${openMenu?"open":"close"}`}>
-                    <img onClick={()=>navegador("")} src={logo} alt="LOGO" />
-                    <div>
-                        <p onClick={()=>navegador("pokedex")}>Pokedex</p>
-                        <p>Legendarios</p>
-                        <p>Sobre mí</p>
-                    </div>
+                onClick={()=>{
+                    window.scrollTo({
+                        top: 0,
+                        behavior: "auto",
+                    });
+                    setOpenMenu(!openMenu)
+                }}
+            >
+                <div className="inner">
+                    <span className={`bar ${menuBar&&"bar2"}`}></span>
+                    <span className={`bar ${menuBar&&"bar2"} ${menuBar&&"bar2-background"}`}></span>
+                    <span className={`bar ${menuBar&&"bar2"}`}></span>
                 </div>
-                <div className={`fondoMenu ${openMenu?"open":"close"}`} onClick={()=>setOpenMenu(!openMenu)}></div>
+            </button>
+            <div className={`menu ${openMenu?"open":"close"}`}>
+                <img onClick={()=>navegador("")} src={logo} alt="LOGO" />
+                <div>
+                    <p onClick={()=>navegador("pokedex")}>Pokedex</p>
+                    <p>Legendarios</p>
+                    <p>Sobre mí</p>
+                </div>
+            </div>
+            <div className={`fondoAlt ${openMenu?"open":"close"}`} onClick={()=>setOpenMenu(!openMenu)}></div>
         </div>
     )
 }
