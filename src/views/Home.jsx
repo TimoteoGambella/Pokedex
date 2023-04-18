3f8c198e8a07c8227b4176a00e08e1b696077cfe
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-04-18 10:58:55 -0300

    limpio

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,23 +1,14 @@
-import { useContext, useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {  UseApiContext } from "../context/ApiContext";
 import banner from "../assets/banner.svg"
 
 export default function Home(){
     const navigate=useNavigate()
-    const {generation}=useParams()
     const { colorsTypes, apiPoke, allPokes } = useContext(UseApiContext)
 
     const [generationPokes,setGenerationPokes]=useState([])
 
-//     useEffect(() => {
-//         if(generation!==undefined){
-//             apiPoke(`https://pokeapi.co/api/v2/generation/${generation}`).then((res)=>setGenerationPokes(res))
-//         }
-        
-//         // eslint-disable-next-line react-hooks/exhaustive-deps
-//     }, []);
-// console.log(generationPokes)
     return(
         <div className="home-container">
             <img src={banner} alt="BANNER" />
