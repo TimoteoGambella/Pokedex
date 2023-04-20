abe33d5881de91b1ba770d8d1d4e51dce7ac3a78
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-04-20 11:19:22 -0300

    pokes

--- src/components/CardsContent.jsx
+++ src/components/CardsContent.jsx
@@ -2,13 +2,20 @@ import { Fragment, useContext, useState } from "react"
 import { UseApiContext } from "../context/ApiContext"
 import CardPoke from "./CardPoke"
 
-export default function CardsContent(){
+export default function CardsContent({pokesFilter}){
     const { allPokes } = useContext(UseApiContext)
     const [cargando,setCargando]=useState(true)
 
     return(
         <div className="cards-container">
-            {allPokes.length!==0 && allPokes.results.map((obj,i)=>{
+            {pokesFilter.length===0 && allPokes.length!==0 && allPokes.results.map((obj,i)=>{
+                return(
+                    <Fragment key={i}>
+                        <CardPoke poke={obj} i={i+1===allPokes.results.length} setCargando={setCargando}/>
+                    </Fragment>
+                )
+            })}
+            {pokesFilter.length!==0 && pokesFilter.results.map((obj,i)=>{
                 return(
                     <Fragment key={i}>
                         <CardPoke poke={obj} i={i+1===allPokes.results.length} setCargando={setCargando}/>
