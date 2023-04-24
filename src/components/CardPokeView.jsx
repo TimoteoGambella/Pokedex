557f7119246fc95acf7341da483af8c8e206f229
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-04-24 13:01:39 -0300

    limpieza

--- src/components/CardPokeView.jsx
+++ src/components/CardPokeView.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-export default function CardPokeView({}){
+export default function CardPokeView(){
     const [carga,setCarga]=useState(false)
 
     useEffect(() => {
