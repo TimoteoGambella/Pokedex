09759135cf7afceec21d77295b0bc4bc94149947
TimoteoGambella <timi.gambella@hotmail.com>
2023-04-25 19:52:52 -0300

    bannerHome

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import banner from "../assets/banner.svg"
+import { useContext } from "react";
+import { UseApiContext } from "../context/ApiContext";
 
 export default function Home(){
     const navigate=useNavigate()
+    const {isTablet,isDesktop}=useContext(UseApiContext)
 
     return(
-        <div className="home-container">
-            <img src={banner} alt="BANNER" />
+        <div className={`home-container ${isTablet&&!isDesktop?"tablet":isDesktop?"desktop":""}`}>
+            <img src={banner} alt="BANNER"/>
             <h2><span>Find</span> all your favorite <span>Pokemon</span></h2>
             <h3>You can know the type of Pokemon, its strengths, disadvantages and abilities</h3>
             <p onClick={()=>navigate("/pokedex")}>See Pokemons</p>
