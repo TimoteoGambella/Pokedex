1058b194d3642f92d10d9370b4b8125f83ab08db
TimoteoGambella <timi.gambella@hotmail.com>
2023-04-25 23:54:05 -0300

    filtros

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -9,75 +9,77 @@ export default function Filters({openFilters,setOpenFilters,types,setTypes,gener
 
     return(
         <div className={`filtros-menu ${openFilters?"open":"close"}`}>
-            <button className={`hamburger hamburger--stack ${openFilters&&"active"}`} type="button"
-                onClick={()=>{
-                    window.scrollTo({
-                        top: 0,
-                        behavior: "auto",
-                    });
-                    setOpenFilters(!openFilters)
-                }}
-            >
-                <div className="inner">
-                    <span className={`bar bar2`}></span>
-                    <span className={`bar bar2 bar2-background`}></span>
-                    <span className={`bar bar2`}></span>
+            <div className="subDiv">
+                <button className={`hamburger hamburger--stack ${openFilters&&"active"}`} type="button"
+                    onClick={()=>{
+                        window.scrollTo({
+                            top: 0,
+                            behavior: "auto",
+                        });
+                        setOpenFilters(!openFilters)
+                    }}
+                >
+                    <div className="inner">
+                        <span className={`bar bar2`}></span>
+                        <span className={`bar bar2 bar2-background`}></span>
+                        <span className={`bar bar2`}></span>
+                    </div>
+                </button>
+                
+                <h3>Types</h3>
+                <div className="types">
+                    {allTypes.length !== 0 && allTypes.results.map((obj,i)=>{
+                        return(
+                            <div key={i} onClick={()=>types===obj.name?setTypes(""):setTypes(obj.name)}>
+                                <img src={types===obj.name?radio:radio2} alt="RADIOST" />
+                                <p>{obj.name[0].toUpperCase()}{obj.name.slice(1)}</p>
+                            </div>
+                        )
+                    })}
                 </div>
-            </button>
-            
-            <h3>Types</h3>
-            <div className="types">
-                {allTypes.length !== 0 && allTypes.results.map((obj,i)=>{
-                    return(
-                        <div key={i} onClick={()=>types===obj.name?setTypes(""):setTypes(obj.name)}>
-                            <img src={types===obj.name?radio:radio2} alt="RADIOST" />
-                            <p>{obj.name[0].toUpperCase()}{obj.name.slice(1)}</p>
-                        </div>
-                    )
-                })}
-            </div>
 
-            <h3>Generations</h3>
-            <div className="generations">
-                {allGenerations.length !== 0 && allGenerations.results.map((obj,i)=>{
-                    return(
-                        <div key={i} onClick={()=>generations===obj.name?setGenerations(""):setGenerations(obj.name)}>
-                            <img src={generations===obj.name?radio:radio2} alt="RADIOSG" />
-                            <p>{obj.name[0].toUpperCase()}{obj.name.slice(1)}</p>
-                        </div>
-                    )
-                })}
-            </div>
+                <h3>Generations</h3>
+                <div className="generations">
+                    {allGenerations.length !== 0 && allGenerations.results.map((obj,i)=>{
+                        return(
+                            <div key={i} onClick={()=>generations===obj.name?setGenerations(""):setGenerations(obj.name)}>
+                                <img src={generations===obj.name?radio:radio2} alt="RADIOSG" />
+                                <p>{obj.name[0].toUpperCase()}{obj.name.slice(1)}</p>
+                            </div>
+                        )
+                    })}
+                </div>
 
-            <p className="button" onClick={async()=>{
-                document.getElementById("buscador").value=""
-                await setBuscando(true)
-                if(types!==""||generations!==""){
-                    setPokesFilterBuscador([])
-                    if(types===""&&generations!==""){
-                        await setPokesFilter([])
-                        await apiPoke(`https://pokeapi.co/api/v2/generation/${generations}`).then((res)=>setPokesFilter(res))
-                    }else if(types!==""&&generations===""){
-                        await setPokesFilter([])
-                        await apiPoke(`https://pokeapi.co/api/v2/type/${types}`).then((res)=>setPokesFilter(res))
+                <p className="button" onClick={async()=>{
+                    document.getElementById("buscador").value=""
+                    await setBuscando(true)
+                    if(types!==""||generations!==""){
+                        setPokesFilterBuscador([])
+                        if(types===""&&generations!==""){
+                            await setPokesFilter([])
+                            await apiPoke(`https://pokeapi.co/api/v2/generation/${generations}`).then((res)=>setPokesFilter(res))
+                        }else if(types!==""&&generations===""){
+                            await setPokesFilter([])
+                            await apiPoke(`https://pokeapi.co/api/v2/type/${types}`).then((res)=>setPokesFilter(res))
+                        }else{
+                            await setPokesFilter([])
+                            await apiPoke(`https://pokeapi.co/api/v2/generation/${generations}`).then((res)=>setPokesFilter(res))
+                        }
+                        setOpenFilters(!openFilters)
                     }else{
                         await setPokesFilter([])
-                        await apiPoke(`https://pokeapi.co/api/v2/generation/${generations}`).then((res)=>setPokesFilter(res))
+                        setOpenFilters(!openFilters)
                     }
-                    setOpenFilters(!openFilters)
-                }else{
-                    await setPokesFilter([])
-                    setOpenFilters(!openFilters)
-                }
-                setBuscando(false)
-            }}>Aplicar</p>
-            
-            <p className="button2" onClick={()=>{
-                setOpenFilters(false)
-                setPokesFilter([])
-                setTypes("")
-                setGenerations("")
-            }}>Limpiar filtros</p>
+                    setBuscando(false)
+                }}>Aplicar</p>
+                
+                <p className="button2" onClick={()=>{
+                    setOpenFilters(false)
+                    setPokesFilter([])
+                    setTypes("")
+                    setGenerations("")
+                }}>Limpiar filtros</p>
+            </div>
         </div>  
     )
 }
