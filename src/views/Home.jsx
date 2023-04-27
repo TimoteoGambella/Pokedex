220c579edc364695d1ec94e79be052f9bee203dd
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-04-27 11:45:03 -0300

    bannerDesk

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,17 +2,33 @@ import { useNavigate } from "react-router-dom";
 import banner from "../assets/banner.svg"
 import { useContext } from "react";
 import { UseApiContext } from "../context/ApiContext";
+import { useMediaQuery } from "@mui/material";
 
 export default function Home(){
     const navigate=useNavigate()
     const {isTablet}=useContext(UseApiContext)
 
+    const newBanner=useMediaQuery("(min-width:1300px)")
+
     return(
         <div className={`home-container ${isTablet&&"tablet"}`}>
-            <img src={banner} alt="BANNER"/>
-            <h2><span>Find</span> all your favorite <span>Pokemon</span></h2>
-            <h3>You can know the type of Pokemon, its strengths, disadvantages and abilities</h3>
-            <p onClick={()=>navigate("/pokedex")}>See Pokemons</p>
+            {!newBanner ?
+                <>
+                    <img src={banner} alt="BANNER"/>
+                    <h2><span>Find</span> all your favorite <span>Pokemon</span></h2>
+                    <h3>You can know the type of Pokemon, its strengths, disadvantages and abilities</h3>
+                    <p onClick={()=>navigate("/pokedex")}>See Pokemons</p>
+                </>
+                :
+                <div className="newBanner">
+                    <div>
+                        <h2><span>Find</span> all your favorite <span>Pokemon</span></h2>
+                        <h3>You can know the type of Pokemon, its strengths, disadvantages and abilities</h3>
+                        <p onClick={()=>navigate("/pokedex")}>See Pokemons</p>
+                    </div>
+                    <img src={banner} alt="BANNER"/>
+                </div>
+            }
         </div>
     )
 }
